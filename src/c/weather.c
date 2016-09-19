9a5a920b77bc25a2d257632c7338e844cde5b34e
GreenHex <GreenHex@gmail.com>
2016-09-19 10:35:08 +0530

    Weather is now working

--- src/c/weather.c
+++ src/c/weather.c
@@ -2,44 +2,65 @@
 #include "base.h"
 #include "weather.h"
 
-static TextLayer *weather_layer;
-static weather_t weather;
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
 
-static char* weather_condition(int icon) {
-  switch (icon) {
-    case 1: return "%d° clear";
-    case 2: return "%d° cloudy";
-    case 4: return "%d° rainy";
-    case 5: return "%d° stormy";
-    case 6: return "%d° snowy";
-    case 7: return "%d° misty";
-    default: return "%d° ---";
+// ----
+
+static TextLayer *s_weather_layer;
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%d°C", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
   }
 }
 
-void receive_weather_data(int icon, int temp) {
-  weather.icon = icon;
-  weather.temp = temp;
-
-  char *temp_string = "XXXXX";
-  char *condition = weather_condition(weather.icon);
-
-  snprintf(temp_string, sizeof("-137x stormy"), condition, weather.temp);
-  text_layer_set_text(weather_layer, temp_string);
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
 
-void weather_init() {
-  weather = (weather_t) {
-    .icon = 0,
-    .temp = 0
-  };
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
 
-  weather_layer = text_layer_create(GRect(2,-3,107,23));
-  setup_text_layer(weather_layer, RESOURCE_ID_FONT_DROIDSANS_18);
-  text_layer_set_text(weather_layer, "--");
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+// ----
+
+
+void weather_init() {
+  s_weather_layer = text_layer_create(GRect(2, 1, 107, 16)); // GRect(110,1,34,16));
+  setup_text_layer(s_weather_layer, RESOURCE_ID_FONT_DROIDSANS_12);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 void weather_deinit() {
-  text_layer_destroy(weather_layer);
+  text_layer_destroy(s_weather_layer);
   app_message_deregister_callbacks();
 }
--- src/c/weather.h
+++ src/c/weather.h
@@ -1,10 +1,4 @@
 #pragma once
 
-typedef struct {
-  int icon;
-  int temp;
-} weather_t;
-
-void receive_weather_data(int, int);
 void weather_init(void);
 void weather_deinit(void);
