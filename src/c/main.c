59591fbe44045bfbc17120182017f86eb449593a
GreenHex <GreenHex@gmail.com>
2016-10-13 11:06:53 +0530

    Still allocated <0B>
    Finally... the magic "0B"

--- src/c/main.c
+++ src/c/main.c
@@ -8,6 +8,7 @@
 #include "chime.h"
 #include "app_messaging.h"
 
+static Window *window;
 
 static void window_load( Window *window ) { // order is important as layers are drawn over each other.
   calendar_init( window );
@@ -25,7 +26,7 @@ static void window_unload( Window *window ) {
   calendar_deinit();
 }
 
-static void init( Window *window ) {
+static void init( void ) {
   window = window_create();
   window_set_window_handlers( window, ( WindowHandlers ) {
     .load = window_load,
@@ -34,18 +35,17 @@ static void init( Window *window ) {
   window_stack_push( window, false );
 }
 
-static void destroy( Window *window ) {
+static void destroy( void ) {
   window_destroy( window );
 }
 
 int main( void ) {
-  static Window *window;
   
-  init( window );
+  init();
   
   APP_LOG( APP_LOG_LEVEL_DEBUG, "After init(): Heap: %d bytes used, %d bytes free", (int) heap_bytes_used(), (int) heap_bytes_free() );  
   app_event_loop();
-  destroy( window );
+  destroy();
   APP_LOG( APP_LOG_LEVEL_DEBUG, "After window_destroy() Heap: %d bytes used, %d bytes free", (int) heap_bytes_used(), (int) heap_bytes_free() );
   
   return 0;
